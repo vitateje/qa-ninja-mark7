e38bcdc62e024402ffd26ae00a967ebcd9c716f1
vitateje <vitateje@gmail.com>
2021-03-15 13:56:23 -0300

    refatoração-com-esquema-de-cenario

--- mark7/features/support/hooks.rb
+++ mark7/features/support/hooks.rb
@@ -1,6 +1,11 @@
 
-# Toda vez que um cenario com a tag @logout for executado, a função abaixo será executada após a execução do cenario.
+# Toda vez que um cenario com a tag @logout for executado, a função abaixo será executada após (After) a execução do cenario.
 After('@logout') do 
     click_link @email
     click_link 'Sair'
 end
+
+
+# Before
+# BeforeSteps
+# AfterSteps
