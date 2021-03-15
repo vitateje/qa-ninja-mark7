1d051d3d5a864dd4326492abd0e1033b66a25500
vitateje <vitateje@gmail.com>
2021-03-15 12:13:36 -0300

    contexto

--- mark7/features/step_definitions/login_steps.rb
+++ mark7/features/step_definitions/login_steps.rb
@@ -26,7 +26,3 @@
         expect(page).to have_content mensagem
     end
     
-    # Toda vez que um cenario com a tag @logout for executado, a função abaixo será executada após a execução do cenario.
-    after('@logout') do 
-        click_link @email
-    end
--- mark7/features/support/hooks.rb
+++ mark7/features/support/hooks.rb
@@ -1,5 +1,6 @@
 
 # Toda vez que um cenario com a tag @logout for executado, a função abaixo será executada após a execução do cenario.
-after('@logout') do 
+After('@logout') do 
     click_link @email
+    click_link 'Sair'
 end
